9fe733ad3e042d83d29f1e47a57d044da8eb773f
Erastus Amunwe <eneamunwe@gmail.com>
2021-10-17 20:01:50 +0200

    Implemented the user-info route handler

--- protected-resource.js
+++ protected-resource.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require("fs")
+const jwt = require("jsonwebtoken")
 const { timeout } = require("./utils")
 
 const config = {
@@ -31,6 +32,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+app.get("/user-info", (req, res) => {
+	if(!req.headers.authorization)
+		return res.status(401).end()
+
+	const token = req.headers.authorization.slice(7)
+	let decoded ;
+	try {
+		decoded = jwt.verify(token, config.publicKey, { algorithms: ["RS256"]})
+		
+	} catch (error) {
+		return res.status(401).end()
+	}
+
+	const scopes = decoded.scope.split(" ")
+	const userInfo = {}
+	scopes.forEach(element => {
+		let propertyName = element.slice(12)
+		userInfo[propertyName] = users[decoded.userName][propertyName]
+	});
+	res.status(200).json(userInfo)
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
